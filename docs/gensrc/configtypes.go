34cee0a03270f9fea3578288a1f328a43b9d9de6
Daniel Nephin <dnephin@gmail.com>
2016-07-17 17:30:45 -0400

    Add a script to generate docs for config types.
    Signed-off-by: Daniel Nephin <dnephin@gmail.com>

---
+++ docs/gensrc/configtypes.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/dnephin/dobi/config"
+	"github.com/dnephin/dobi/config/docs"
+)
+
+var (
+	basePath = "docs/gen/config/"
+)
+
+func write(filepath string, source interface{}) error {
+	content, err := docs.Generate(source, docs.ReStructuredText)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filepath, bytes.NewBufferString(content).Bytes(), 0644)
+}
+
+func writeDocs() error {
+	for _, item := range []struct {
+		filename string
+		source   interface{}
+	}{
+		{"meta.rst", config.MetaConfig{}},
+		{"alias.rst", config.AliasConfig{}},
+		{"compose.rst", config.ComposeConfig{}},
+		{"image.rst", config.ImageConfig{}},
+		{"mount.rst", config.MountConfig{}},
+		{"run.rst", config.RunConfig{}},
+	} {
+		fmt.Printf("Generating doc %q\n", basePath+item.filename)
+		if err := write(basePath+item.filename, item.source); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	if err := writeDocs(); err != nil {
+		fmt.Printf("Failed to generate docs: %s\n", err)
+		os.Exit(1)
+	}
+}
