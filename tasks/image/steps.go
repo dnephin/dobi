dc92c98c717c3457f18981bea7e053b6a5cd57c7
cescoferraro <francescoaferraro@gmail.com>
2016-11-06 14:04:38 -0200

    little spaces; + weird char

--- tasks/image/steps.go
+++ tasks/image/steps.go
@@ -9,7 +9,7 @@ import (
 
 type stepInjector string
 
-var customSteṕs = map[string]string{
+var customSteps = map[string]string{
 	"GLIDE":  "{{ .GLIDE }}",
 	"GO-GET": "{{ .GOGET }}",
 	"GO-BIN": "{{.GOBIN}}",
@@ -26,7 +26,7 @@ func (step stepInjector) GLIDE() string {
 func (step stepInjector) GOBIN() string {
 	strps := strings.Split(string(step), "/")
 	return "go get -u " + string(step) + " && cp /go/bin/" +
-		strps[len(strps)-1] + "/usr/bin/ && rm -rf /go/src/* /go/pkg/* /go/bin/*"
+		strps[len(strps)-1] + " /usr/bin/ && rm -rf /go/src/* /go/pkg/* /go/bin/*"
 }
 
 func (step stepInjector) GOGET() string {
@@ -35,11 +35,10 @@ func (step stepInjector) GOGET() string {
 
 func (t *Task) templateStep(key string, STEP map[string]string) error {
 	buf := new(bytes.Buffer)
-	tmpl, err := template.New("").Parse(customSteṕs[key])
+	tmpl, err := template.New("").Parse(customSteps[key])
 	if err != nil {
 		return err
 	}
-
 	err = tmpl.Execute(buf, stepInjector(STEP[key]))
 	if err != nil {
 		return err
@@ -56,7 +55,7 @@ func (t *Task) templateStep(key string, STEP map[string]string) error {
 func (t *Task) replaceCustomSteps() error {
 	for _, STEP := range t.config.Steps {
 		for key := range STEP {
-			if _, ok := customSteṕs[key]; ok {
+			if _, ok := customSteps[key]; ok {
 				err := t.templateStep(key, STEP)
 				if err != nil {
 					return fmt.Errorf("template step"+key+"s failed with error", err)
