f8b7bf1e778132354e16f66940fcb791c053862d
cescoferraro <francescoaferraro@gmail.com>
2016-11-22 09:40:31 -0200

    test fix II.

--- config/image.go
+++ config/image.go
@@ -83,8 +83,6 @@ func (c *ImageConfig) validateBuildOrPull() error {
 		if c.Dockerfile != "" || len(c.Context) != 0 {
 			return fmt.Errorf("is pull is set, you cannot specifie a dockerfile or steps")
 		}
-	case len(c.Steps) == 0 && c.Dockerfile == "":
-		return fmt.Errorf("use either a dockerfile or set your steps")
 	case len(c.Steps) != 0 && c.Dockerfile != "":
 		return fmt.Errorf("cannot use both a dockerfile and content")
 	}
