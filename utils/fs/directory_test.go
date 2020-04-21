3eb0f30f597ab11560deb2a9bbbc4262e785e0c5
Tom Duffield <tom@chef.io>
2020-04-20 21:11:23 -0500

    Use file libraries to properly filter excludes
    After adding testing, we discovered that the current method of filtering
    only works if you're filtering top-level files or directories, which
    satisfied the use case of skippiing the '.git' directory.
    
    This add testing, shifts to passing search parameters as a struct, and
    uses proper pattern matching to evaluate the dockerignore file based
    on the Docker documentation.
    
    Signed-off-by: Tom Duffield <tom@chef.io>

--- utils/fs/directory_test.go
+++ utils/fs/directory_test.go
@@ -21,10 +21,62 @@ func TestLastModified(t *testing.T) {
 		mtime := time.Now().AddDate(0, 0, index+10)
 		assert.Assert(t, cmp.Nil(touch(tmpdir.Join(dir, "file"), mtime)))
 
-		actual, err := LastModified(nil, tmpdir.Path())
+		actual, err := LastModified(&LastModifiedSearch{
+			Root:  tmpdir.Path(),
+			Paths: []string{tmpdir.Path()},
+		})
 		assert.NilError(t, err)
 		assert.Equal(t, actual, mtime)
 	}
+}
+
+func TestLastModifiedExcludesFile(t *testing.T) {
+	tmpdir := fs.NewDir(t, "test-directory-last-modified-excludes-file",
+		fs.WithDir("a"),
+		fs.WithDir("b",
+			fs.WithDir("c")))
+	defer tmpdir.Remove()
+
+	for index, dir := range []string{"a", "b", "b/c"} {
+		mtime := time.Now().AddDate(0, 0, index+10)
+		assert.Assert(t, cmp.Nil(touch(tmpdir.Join(dir, "file"), mtime)))
+
+		excludedFile := tmpdir.Join(dir, "excluded-file")
+		excludedMtime := time.Now().AddDate(0, 0, index+20)
+		assert.Assert(t, cmp.Nil(touch(excludedFile, excludedMtime)))
+
+		actual, err := LastModified(&LastModifiedSearch{
+			Root:     tmpdir.Path(),
+			Excludes: []string{"**/**/excluded-file"},
+			Paths:    []string{tmpdir.Path()},
+		})
+		assert.NilError(t, err)
+		assert.Equal(t, actual, mtime)
+	}
+}
+
+func TestLastModifiedExcludesFolder(t *testing.T) {
+	tmpdir := fs.NewDir(t, "test-directory-last-modified-excludes-folder",
+		fs.WithDir("a"),
+		fs.WithDir("b",
+			fs.WithDir("c")))
+	defer tmpdir.Remove()
+
+	mtime := time.Now().AddDate(0, 0, 0)
+	assert.Assert(t, cmp.Nil(touch(tmpdir.Join("a", "file"), mtime)))
+
+	for index, dir := range []string{"b", "b/c"} {
+		ignoredMtime := time.Now().AddDate(0, 0, index+10)
+		assert.Assert(t, cmp.Nil(touch(tmpdir.Join(dir, "file"), ignoredMtime)))
+	}
+
+	actual, err := LastModified(&LastModifiedSearch{
+		Excludes: []string{"b/"},
+		Paths:    []string{tmpdir.Path()},
+		Root:     tmpdir.Path(),
+	})
+	assert.NilError(t, err)
+	assert.Equal(t, actual, mtime)
 }
 
 func touch(name string, mtime time.Time) error {
